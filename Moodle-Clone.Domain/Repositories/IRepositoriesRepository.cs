121dd23a2653f1bfc29f7a400ec68cd70fa4e9e5
galileo680 <bartek.wicz@gmail.com>
2024-04-19 17:29:29 +0200

    RepositoriesRepository Added

---
+++ Moodle-Clone.Domain/Repositories/IRepositoriesRepository.cs
@@ -0,0 +1,12 @@
+﻿using MoodleClone.Domain.Entities;
+
+namespace MoodleClone.Domain.Repositories;
+
+public interface IRepositoriesRepository
+{
+    Task<IEnumerable<Repository>> GetAllAsync();
+    Task<Repository?> GetByIdAsync(int id);
+    Task<int> Create(Repository entity);
+    Task Delete(Repository entity);
+    Task SaveChanges();
+}
---
+++ Moodle-Clone.Infrastructure/Repositories/RepositoriesRepository.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MoodleClone.Domain.Entities;
+using MoodleClone.Domain.Repositories;
+using MoodleClone.Infrastructure.Persistence;
+
+namespace MoodleClone.Infrastructure.Repositories;
+
+internal class RepositoriesRepository(MoodleCloneDbContext dbContext) : IRepositoriesRepository
+{
+    public async Task<int> Create(Repository entity)
+    {
+        dbContext.Repositories.Add(entity);
+        await dbContext.SaveChangesAsync();
+        return entity.Id;
+    }
+
+    public async Task Delete(Repository entity)
+    {
+        dbContext.Remove(entity);
+        await dbContext.SaveChangesAsync();
+    }
+
+    public async Task<IEnumerable<Repository>> GetAllAsync()
+    {
+        var repositories = await dbContext.Repositories.ToListAsync();
+        return repositories;
+    }
+
+    public async Task<Repository?> GetByIdAsync(int id)
+    {
+        var repository = await dbContext.Repositories
+            .Include(r => r.Assignments)
+            .ThenInclude(a => a.Submissions)
+            .FirstOrDefaultAsync(x => x.Id == id);
+        return repository;
+    }
+
+    public Task SaveChanges()
+        => dbContext.SaveChangesAsync();
+}
